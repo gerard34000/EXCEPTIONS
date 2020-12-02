29e0af7c98ac371dfe52406195d9238bd9ddac35
gerard34000 <33629854+gerard34000@users.noreply.github.com>
2020-12-02 12:05:32 +0100

    Add files via upload

---
+++ TP11-Exceptions/src/TryCatch.java
@@ -0,0 +1,19 @@
+
+public class TryCatch {
+
+	public TryCatch() {
+		// TODO Auto-generated constructor stub
+		// TODO Auto-generated method stub
+		// EXCEPTION :IL MANQUE UN MAIN !!!!!
+				int somme= 0;
+				for (int i = 0; i < 10; i++) {
+					somme += i;
+				}
+				System.out.println("Ma Somme " + somme); 
+	}
+
+	
+		
+	
+
+}
